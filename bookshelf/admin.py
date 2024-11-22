b671c39a1325947e0e5fb5831271eef3d4017ccd
Jairo Chapela-Martínez <jairochapela@gmail.com>
2024-11-22 13:51:04 +0100

    Modelos y administrador.

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from bookshelf.models import Autor, Libro, Resena
 
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Libro)
+admin.site.register(Resena)
--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=80)
+
+GENEROS = [
+    ('aventuras', 'Aventuras'),
+    ('ciencia_ficcion', 'Ciencia ficción'),
+    ('fantasia', 'Fantasía'),
+    ('epica', 'Épica'),
+    ('ensayo', 'Ensayo'),
+    ('terror', 'Terror'),
+    ('misterio', 'Misterio'),
+    ('romance', 'Romance'),
+    ('drama', 'Drama'),
+    ('comedia', 'Comedia'),
+    ('infantil', 'Infantil'),
+    ('juvenil', 'Juvenil'),
+    ('poesia', 'Poesía'),
+    ('historia', 'Historia'),
+    ('biografia', 'Biografía'),
+]
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autores = models.ManyToManyField(Autor)
+    genero = models.CharField(max_length=50, choices=GENEROS)
+    fecha_pub = models.DateField()
+    bibliotecario = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)                     
+
+
+class Resena(models.Model):
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    usuario = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    
